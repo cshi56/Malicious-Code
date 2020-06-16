94ced26ef44cf5caab123df69ff42ee9839a4aa8
Joseph Cusano <owen.astrachan@duke.edu>
2020-06-16 14:04:01 -0400

    Started compiling yara rules and created gui for selecting files to text with yara rules

--- yaraTests.py
+++ yaraTests.py
@@ -1,9 +1,14 @@
 import yara
+from tkinter.filedialog import askopenfilename
 
 def mycallback(data):
-    print(data)
+    print("Rule: " + str(data['rule']))
+    print("Description: " + data['meta']['description'])
+    print("Match?: " + str(data['matches']))
     return yara.CALLBACK_CONTINUE
 
+filename = askopenfilename()
+
 rules = yara.compile(filepath='/Users/joecus1/Desktop/Yara-Rules/rules/maldocs/Maldoc_VBA_macro_code.yar')
-matches = rules.match('/Users/joecus1/Desktop/Malware/rule-06.20.doc',
-                      callback=mycallback, which_callbacks=yara.CALLBACK_MATCHES)
+matches = rules.match(filename,
+                      callback=mycallback)
