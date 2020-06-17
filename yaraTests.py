055f04645c5740d507298d024bf00af8f9c7d37b
Joseph Cusano <owen.astrachan@duke.edu>
2020-06-17 10:31:06 -0400

    tried to implement all yaratests for maldocs - did not work

--- yaraTests.py
+++ yaraTests.py
@@ -1,14 +1,26 @@
 import yara
 from tkinter.filedialog import askopenfilename
+import os
 
 def mycallback(data):
-    print("Rule: " + str(data['rule']))
-    print("Description: " + data['meta']['description'])
-    print("Match?: " + str(data['matches']))
+    print(data)
     return yara.CALLBACK_CONTINUE
+
+'''
+directory = '/Users/joecus1/Desktop/Yara-Rules/rules/maldocs/'
+filelist = os.listdir(directory)
+dict = {}
+
+for i in range(len(filelist)):
+    dict["file" + str(i)] = \
+        '/Users/joecus1/Desktop/Yara-Rules/rules/maldocs/' + filelist[i]
+'''
 
 filename = askopenfilename()
 
 rules = yara.compile(filepath='/Users/joecus1/Desktop/Yara-Rules/rules/maldocs/Maldoc_VBA_macro_code.yar')
+
+# rules = yara.compile(filepaths=dict)
+
 matches = rules.match(filename,
-                      callback=mycallback)
+                      callback=mycallback, which_callbacks=yara.CALLBACK_MATCHES)
