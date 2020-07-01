0a7b88003899282fabbeb98dac7a7b9bf41a0cb8
Luke Evans <lae32@duke.edu>
2020-06-30 22:22:40 -0400

    added email notif and modelform

--- django-site/src/test/admin.py
+++ django-site/src/test/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Submission
+from .models import Submission, FileSubmission
 
 # Register your models here.
+# Password is maliciouscode
+# Username is admin
 admin.site.register(Submission)
+admin.site.register(FileSubmission)
